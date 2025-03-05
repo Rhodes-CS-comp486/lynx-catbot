c14f26c36f7178dffe280a7268fc0637b482d3f1
harperkole <harperkole@gmail.com>
2025-03-04 19:13:14 -0600

    Added all campus dining information and converted database to postgres

--- catbot_django/core/models.py
+++ catbot_django/core/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 # Create your models here.
 class FixedContent(models.Model):
+    #id = models.AutoField(primary_key=True)
+    #category = models.CharField(max_length=50)
+    #subcategory = models.CharField(max_length=50)
+    #question = models.CharField(max_length=50)
+    #answer = models.CharField(max_length=800)
     id = models.AutoField(primary_key=True)
-    category = models.CharField(max_length=50)
-    subcategory = models.CharField(max_length=50)
-    question = models.CharField(max_length=50)
-    answer = models.CharField(max_length=800)
+    category = models.TextField()
+    subcategory = models.TextField()
+    question = models.TextField()
+    answer = models.TextField()
+
 
     class Meta:
         verbose_name = "Major Requirements"
